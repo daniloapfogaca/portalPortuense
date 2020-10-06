83082db447593c777b7bfeae524aa4263acb310e
daniloapfogaca <daniloapfogaca@gmail.com>
2020-10-06 16:57:56 -0300

    menu

--- mobile/src/assets/menuData.js
+++ mobile/src/assets/menuData.js
@@ -5,19 +5,101 @@ export default [
     products: [
       {
         productImg: 'img',
-        productName: 'Pão Francês',
+        productName: 'PÃO FRANCÊS',
         productValue: 'R$ 1,00',
         productDescription: 'A lei exige que o Pão Francês seja vendido por kg. Valor aproixmado: o peso do pão varia entre 50 e 60 gramas. O preço do KG é R$ 16,90',
       },
       {
         productImg: 'img',
-        productName: 'Pão Integral',
-        productValue: 'R$ 1,50',
+        productName: 'PÃO INTEGRAL',
+        productValue: 'R$ 1,70',
       },
       {
         productImg: 'img',
-        productName: 'Pão de Hamburguer',
+        productName: 'PÃO DE HAMBURGUÉR',
         productValue: 'R$ 1,70',
+      },
+      {
+        productImg: 'img',
+        productName: 'PÃO DE HOT DOG',
+        productValue: 'R$ 1,00',
+      },
+      {
+        productImg: 'img',
+        productName: 'BAGUETINHA COM GERGELIM',
+        productValue: 'R$ 1,70',
+      },
+      {
+        productImg: 'img',
+        productName: 'PAO ITALIANO GRANDE',
+        productValue: 'R$ 12,00',
+      },
+      {
+        productImg: 'img',
+        productName: 'PAO ITALIANO PEQUENO',
+        productValue: 'R$ 10,50',
+      },
+      {
+        productImg: 'img',
+        productName: 'PAO ITALIANO REDONDO',
+        productValue: 'R$ 10,00',
+      },
+      {
+        productImg: 'img',
+        productName: 'BAGUETE ITALIANA',
+        productValue: 'R$ 12,00',
+      },
+      {
+        productImg: 'img',
+        productName: 'PÃO PORTUGUÊS',
+        productValue: 'R$ 13,00',
+        productDescription: 'A lei exige que o Pão Francês seja vendido por kg. Valor aproixmado: o peso do pão varia entre 50 e 60 gramas. O preço do KG é R$ 16,90',
+      },
+      {
+        productImg: 'img',
+        productName: 'PÃO CIABATTA',
+        productValue: 'R$ 11,00',
+      },
+      {
+        productImg: 'img',
+        productName: 'PÃO DE TORRESMO',
+        productValue: 'R$ 11,00',
+      },
+      {
+        productImg: 'img',
+        productName: 'PÃO DE TORRESMO COM RÚCULA ',
+        productDescription: 'Disponível de quinta a domingo a partir das 14:00horas',        
+        productValue: 'R$ 11,00',
+      },
+      {
+        productImg: 'img',
+        productName: 'PAO QUEIJO GRANDE',
+        productValue: 'R$ 4,20',
+      },
+      {
+        productImg: 'img',
+        productName: 'MINI PÃO DE QUEIJO (6unidades)',
+        productValue: 'R$ 4,20',
+      },
+      {
+        productImg: 'img',
+        productName: 'PAO ITALIANO PEQUENO',
+        productValue: 'R$ 5,00',
+      },
+      {
+        productImg: 'img',
+        productName: 'BAGUETES ITALIANA RECHEADAS',
+        productValue: 'R$ 15,00',
+      },
+      {
+        productImg: 'img',
+        productName: 'BROA DE FUBÁ AEROSA',
+        productValue: 'R$ 3,00',
+      },
+      {
+        productImg: 'img',
+        productName: 'BISNAGA DE CARÁ',
+        productValue: 'R$ 3,00',
       },
     ],
   },
@@ -32,10 +114,303 @@ export default [
       },
       {
         productName: 'Mortadela Ceratti',
+        productValue: 'R$ 6,90',
+      },
+      {
+        productName: 'MORTADELA PERDIGAO OURO',
+        productValue: 'R$ 4,20',
+      },
+      {
+        productName: 'MUSSARELA',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'MUSSARELA LIGHT',
+        productValue: 'R$ 7,50',
+      },
+      {
+        productName: 'PEITO DE PERU DEFUMADO',
+        productValue: 'R$ 7,90',
+      },
+      {
+        productName: 'PRESUNTO COM CAPA SADIA',
+        productValue: 'R$ 4,90',
+      },
+      {
+        productName: 'PRESUNTO ROYALE',
+        productValue: 'R$ 6,90',
+      },
+      {
+        productName: 'PRESUNTO S/ CAPA SADIA',
+        productValue: 'R$ 4,90',
+      },
+      {
+        productName: 'PROVOLONE',
+        productValue: 'R$ 7,00',
+      },
+      {
+        productName: 'QUEIJO BOLA',
+        productValue: 'R$ 7,00',
+      },
+      {
+        productName: 'QUEIJO FRESCO TIROLEZ',
+        productValue: 'R$ 5,50',
+      },
+      {
+        productName: 'QUEIJO PRATO',
+        productValue: 'R$ 6,00',
+      },
+      {
+        productName: 'ROSBIFE',
+        productValue: 'R$ 6,90',
+      },
+      {
+        productName: 'SALAME HAMBURGUÊS SADIA',
+        productValue: 'R$ 9,90',
+      },
+      {
+        productName: 'SALAME ITALIANO AURORA',
+        productValue: 'R$ 9,90',
+      },
+      {
+        productName: 'SALAME ITALIANO SADIA',
         productValue: 'R$ 9,90',
       }
     ],
-  }
+  },
 
+  {
+    index: 3,
+    category: 'PATÊS (POTE APROX. 150g)',
+    products: [
+      {
+        productName: 'ABOBRINHA GRELHADA',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'ALHO COM SALSA',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'ALICHELA',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'AZEITONA',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'AZEITONA CHILENA PICADA COM PROVOLONE',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'BERINJELA ITALIANA',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'BERINJELA SICILIANA',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'CAPONATA',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'CHAMPINGNON COM PROVOLONE E TOMATE SECO',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'GORGONZOLA',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'ALHO PORÓ COM PROVOLONE E AZEITONA PRETA',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'QUATRO QUEIJOS',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'SARDELA',
+        productValue: 'R$ 14,00',
+      },
+      {
+        productName: 'TOMATE SECO',
+        productValue: 'R$ 14,00',
+      }
+    ],
+  },
 
+  {
+    index: 4,
+    category: 'Salgados',
+    products: [
+      {
+        productName: 'BOLINHO:BACALHAU',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'BOLINHO:CARNE COM AZEITONA',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'BOLINHO:CARNE COM OVO',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'COXINHA',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'COXINHA COM CATUPIRY',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'EMPADA DE PALMITO',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'EMPADA DE FRANGO (A partir das 14:00horas)',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'CROISSANT:PRESUNTO E QUEIJO',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'CROISSANT:QUEIJO BRANCO',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'ESFIHA:FRANGO',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'ESFIHA:CALABRESA',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'ESFIHA:CARNE',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'KIBE RECHEADO COM CARNE E AZEITONA (A partir das 14:00horas)',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'TRANÇINHA DE PRESUNTO E QUEIJO (A partir das 14:00horas)',
+        productValue: 'R$ 6,50',
+      }
+    ],
+  },
+
+  {
+    index: 5,
+    category: 'TORTAS SALGADAS',
+    products: [
+      {
+        productName: 'FRANGO',
+        productValue: 'R$ 20,00',
+      },
+      {
+        productName: 'PALMITO',
+        productValue: 'R$ 20,00',
+      }
+    ],
+  },
+
+  {
+    index: 6,
+    category: 'DOCES DE PADARIA',
+    products: [
+      {
+        productName: 'CAROLINA CROCANTE (6un.)',
+        productValue: 'R$ 7,00',
+      },
+      {
+        productName: 'CAROLINA DE DOCE DE LEITE (6un.)',
+        productValue: 'R$ 7,00',
+      },
+      {
+        productName: 'CAROLINA DE MORANGO (6un.)',
+        productValue: 'R$ 7,00',
+      },
+      {
+        productName: 'MINI CROISSANT DE CREME (6un.)',
+        productValue: 'R$ 7,00',
+        productDescription:'(Disponível a partir das 14:00Horas)',
+      },
+      {
+        productName: 'DELICIA DE MACA',
+        productValue: 'R$ 6,50',
+        productDescription:'(Disponível a partir das 14:00Horas)',
+      },
+      {
+        productName: 'MINI SONHO RECHEADO (6un.)',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'PÃO DOCE DE CREME',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'PÃO DOCE DE RICOTA',
+        productValue: 'R$ 6,50',
+        productDescription:'(Disponível a partir das 14:00Horas)',
+      },
+      {
+        productName: 'PAO BEBE RECHEADO',
+        productValue: 'R$1.20',
+        productDescription:'(Disponível a partir das 14:00Horas)',
+      },
+      {
+        productName: 'RAINHA DE SABÁ CANELA',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'RAINHA DE SABÁ DE COCO',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'SONHO',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'CROISSANT DOCE:CREME',
+        productValue: 'R$ 6,50',
+      },
+      {
+        productName: 'CROISSANT DOCE:CHOCOLATE]',
+        productValue: 'R$ 6,50',
+        productDescription:'(Disponível a partir das 14:00Horas)',
+      },
+      {
+        productName: 'CROISSANT DOCE:GOIABADA',
+        productValue: 'R$ 6,50',
+        productDescription:'(Disponível a partir das 14:00Horas)',
+      },
+      {
+        productName: 'CROISSANT DOCE:ABACAXI',
+        productValue: 'R$ 6,50',
+        productDescription:'(Disponível a partir das 14:00Horas)',
+      }
+    ],
+  },
+
+  {
+    index: 7,
+    category: 'TORTAS SALGADAS',
+    products: [
+      {
+        productName: 'FRANGO',
+        productValue: 'R$ 20,00',
+      },
+      {
+        productName: 'PALMITO',
+        productValue: 'R$ 20,00',
+      }
+    ],
+  },
+      
 ];
