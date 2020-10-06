2084173c88391f26bbe561e06b05daad8b0b7b0f
jackyfialho <teixeijf@gmail.com>
2020-10-06 16:14:10 -0300

    problema do input resolvido

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TextInput, TouchableOpacity, KeyboardAvoidingView, ScrollView } from 'react-native';
 import { AntDesign, FontAwesome } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 
@@ -19,48 +19,60 @@ const Detail = () => {
   }
 
   return(
-    <View style={styles.container}>
-      <TouchableOpacity style={styles.productNameContainer} onPress={navigateBack} activeOpacity={0.8}>
-        <AntDesign style={styles.goBackIcon} name="left" size={20} color="white" />
-        <Text style={styles.productTitle}>Americano</Text>
-      </TouchableOpacity>
-      <View>
-        <Image style={styles.detailImg} source={Lanche}/>
-      </View>
-      <View style={styles.productInfo}>
-        <Text style={styles.productCategory}>Lanches</Text>
-        <Text style={styles.productName}>Americano</Text>
-        <Text style={styles.productDescription}>Pão Francês, Ovos, Presunto, Queijo tipo mussarela, Alface, Tomate, Maionese, Catchup, Mostarda</Text>
-      </View>
-      <View style={styles.productPurchaseInfo}>
-        <View style={styles.productValue}>
-          <Text style={styles.productValueText}>Valor unitário: </Text>
-          <Text style={styles.productBold}>R$ 16,00</Text>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : null}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+    >
+      <ScrollView>
+        <View style={styles.container}>
+          <TouchableOpacity style={styles.productNameContainer} onPress={navigateBack} activeOpacity={0.8}>
+            <AntDesign style={styles.goBackIcon} name="left" size={20} color="white" />
+            <Text style={styles.productTitle}>Americano</Text>
+          </TouchableOpacity>
+          <View>
+            <Image style={styles.detailImg} source={Lanche}/>
+          </View>
+          <View style={styles.productInfo}>
+            <Text style={styles.productCategory}>Lanches</Text>
+            <Text style={styles.productName}>Americano</Text>
+            <Text style={styles.productDescription}>Pão Francês, Ovos, Presunto, Queijo tipo mussarela, Alface, Tomate, Maionese, Catchup, Mostarda</Text>
+          </View>
+          <View style={styles.productPurchaseInfo}>
+            <View style={styles.productValue}>
+              <Text style={styles.productValueText}>Valor unitário: </Text>
+              <Text style={styles.productBold}>R$ 16,00</Text>
+            </View>
+            <View style={styles.productQuantity}>
+              <FontAwesome name="minus-circle" size={24} color="red" />
+              <Text style={styles.productQuantityValue}>1</Text>
+              <FontAwesome name="plus-circle" size={24} color="green" />
+            </View>
+          </View>
+          <View style={styles.productTotal}>
+            <Text style={styles.productBold}>Total:</Text>
+            <Text style={styles.productTotalValue}>R$ 16,00</Text>
+          </View>
+          <KeyboardAvoidingView
+            style={styles.productObs}
+            behavior={Platform.OS === "ios" ? "padding" : null}
+            keyboardVerticalOffset={Platform.OS === "ios" ? 64 : 0}
+          >
+            <TextInput
+              multiline={true}
+              numberOfLines={4}
+              style={styles.productObsText}
+              placeholder="Observações"
+            />
+          </KeyboardAvoidingView>
+          <View style={styles.addCartContainer}>
+            <TouchableOpacity style={styles.addCart} onPress={navigateToCart} activeOpacity={0.8}>
+              <Text style={styles.addCartText}>ADICIONAR AO CARRINHO</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={styles.productQuantity}>
-          <FontAwesome name="minus-circle" size={24} color="red" />
-          <Text style={styles.productQuantityValue}>1</Text>
-          <FontAwesome name="plus-circle" size={24} color="green" />
-        </View>
-      </View>
-      <View style={styles.productTotal}>
-        <Text style={styles.productBold}>Total:</Text>
-        <Text style={styles.productTotalValue}>R$ 16,00</Text>
-      </View>
-      <View style={styles.productObs}>
-        <TextInput
-          multiline={true}
-          numberOfLines={4}
-          style={styles.productObsText}
-          placeholder="Observações"
-        />
-      </View>
-      <View style={styles.addCartContainer}>
-        <TouchableOpacity style={styles.addCart} onPress={navigateToCart} activeOpacity={0.8}>
-          <Text style={styles.addCartText}>ADICIONAR AO CARRINHO</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+    
   )
 }
 
